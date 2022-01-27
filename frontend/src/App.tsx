ecbad827161721f064487ae5b395e0b1304d0e3f
Akihito Ihara <akiteruto@gmail.com>
2022-01-27 23:12:37 +0900

    fix app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -34,6 +34,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Working on ECS</p>
         <a
           className="App-link"
           href="https://reactjs.org"
