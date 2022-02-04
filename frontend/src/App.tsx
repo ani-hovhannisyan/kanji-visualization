2287061572df9d376daadb95e9a62c35f707ba22
Akihito Ihara <akiteruto@gmail.com>
2022-02-04 15:34:13 +0900

    add states to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,23 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import SearchField from "./SearchField/SearchField";
+
+type kanjiInfo = {
+  onyomi: string;
+  kunyomi: string;
+};
+
+type graphInfo = {
+  graph: any;
+};
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    if (!process.env.REACT_APP_API_URL || !process.env.REACT_APP_API_PORT) {
-      console.error("URL of API is undefined");
-      return;
-    }
-
-    fetch(
-      `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        mode: "cors",
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => console.error(err));
-  }, []);
+  const [kanji, setKanji] = useState<kanjiInfo>();
+  const [graph, setGraph] = useState<graphInfo>();
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>{message}</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Kanji Visualizer</h1>
+      <SearchField setKanji={setKanji} setGraph={setGraph}></SearchField>
     </div>
   );
 }
