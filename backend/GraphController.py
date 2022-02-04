1125f5bf6e690f2c10679035712857495fdd5a66
Ani Hovhannisyan <anihovhannisyang@gmail.com>
2022-02-04 13:10:58 +0900

    Adding empty methods in GraphController following Crass Diagram #36.

--- backend/GraphController.py
+++ backend/GraphController.py
@@ -1,1 +1,22 @@
 # This is the Graph Controller class implementation.
+
+
+class GraphController:
+    def __init__(self):
+        self.MainNode
+        self.FriendNodes = []
+        self.Edges = []
+
+    def set_main_node(self):
+        # TODO: Implement logic
+        return True
+
+    def get_friend_nodes(self):
+        nodes = []
+        # TODO: Implement logic
+        return nodes
+
+    def construct_nodes_matrix(self):
+        matrix = []
+        # TODO: Implement logic
+        return matrix
