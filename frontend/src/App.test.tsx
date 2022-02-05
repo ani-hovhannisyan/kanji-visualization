e619e951ed633b6af95ead6febff5d7dbfa978f0
Shuhei Kayawari <dev.wowry@gmail.com>
2022-02-06 06:25:53 +0900

    Update App.test to support canvas elements

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import "jest-canvas-mock";
 import App from "./App";
 
 process.env.REACT_APP_API_URL = "http://localhost";
