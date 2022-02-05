643562f1b0bf097594ed2343a9704d02ccb31e51
Akihito Ihara <akihito@AkihitonoMacBook-Pro-2.local>
2022-02-05 22:38:45 +0900

    fix pytest

--- backend/tests/test_searchController.py
+++ backend/tests/test_searchController.py
@@ -8,13 +8,13 @@ def test_normal():
 
 def test_invalid1():
     is_success, error_info = SearchController.check_input("a")
-    assert is_success
+    assert not is_success
     assert error_info == {"status_code": 400, "detail": "Only Kanji is permitted"}
 
 
 def test_invalid2():
     is_success, error_info = SearchController.check_input("")
-    assert is_success
+    assert not is_success
     assert error_info == {
         "status_code": 400,
         "detail": "Please include Kanji in the query parameters",
@@ -23,5 +23,5 @@ def test_invalid2():
 
 def test_invalid3():
     is_success, error_info = SearchController.check_input("登山")
-    assert is_success
+    assert not is_success
     assert error_info == {"status_code": 400, "detail": "Only one character is allowed"}
