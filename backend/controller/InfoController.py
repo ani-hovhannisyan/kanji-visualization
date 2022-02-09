3368901c25791082d4eb0aed62e5a7cd6185b38f
TakumiHiraide <84693250+TakumiHiraide@users.noreply.github.com>
2022-02-09 10:11:00 +0900

    I added fixed InfoCotroller.py
    I wrote a code that takes a kanji character as an inout and returns its meaning and reading.

--- backend/controller/InfoController.py
+++ backend/controller/InfoController.py
@@ -1,10 +1,31 @@
+from jamdict import Jamdict
+jam = Jamdict()
 class InfoController:
     @staticmethod
     def get_kanji_info(kanji: str):
         # TODO: return acutual value
-        try:
-            kanji_info = {"onyomi": "さん", "detail": "やま"}
+        query = kanji
+        result = jam.lookup(query)
+        print(result)
+        index = -1
+        for i, c in enumerate(result.chars):
+          print(i,c)
+          if str(c) == query:
+            index = i
+        if i > -1:
+            yomi = []
+            for entry in result.entries:
+              for kana in entry.to_dict()["kana"]:
+                yomi.append(kana["text"])
+            print(yomi)
+            meaning = []
+            for mean in result.chars[index].meanings(english_only=True):  # return only English meaning
+                meaning.append(mean)
+            kanji_info = ["yomi",yomi,"meaning",meaning]
+            #kanji_info = {"onyomi": "さん", "detail": "やま"}
             return [True, None, kanji_info]
-        except Exception:
-            error_info = {"status_code": 400, "detail": "山 is not in data base"}
+        else:
+            error_info = ["status_code", 400, "detail","kanji is not in data base"]
             return [False, error_info, None]
+
+print(InfoController.get_kanji_info("人"))
